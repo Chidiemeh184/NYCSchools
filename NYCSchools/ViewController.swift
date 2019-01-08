f6e9e056e377f56d9b56b92466358bc6fce68a36
Chidi Emeh <chidiemeh184@gmail.com>
2019-01-07 22:40:05 -0500

    Initial Commit

---
+++ NYCSchools/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NYCSchools
+//
+//  Created by Chidi Emeh on 1/7/19.
+//  Copyright © 2019 Chidi Emeh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
